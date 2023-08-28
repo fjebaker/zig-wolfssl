4460fad4a5236f12a257c045ca84e118056a9cfc
fjebaker <fergusbkr@gmail.com>
2023-08-28 17:08:15 +0100

    fix: ensure read / write have known error sets

--- src/SslConnection.zig
+++ src/SslConnection.zig
@@ -27,18 +27,30 @@ pub fn writer(self: *SslConnection) Writer {
     return .{ .context = self };
 }
 
-pub fn read(self: *SslConnection, buffer: []u8) !usize {
+pub fn read(self: *SslConnection, buffer: []u8) ReadError!usize {
     const len = c.wolfSSL_read(self.ssl, buffer.ptr, @as(c_int, @intCast(buffer.len)));
     if (len < 0) {
-        try codes.retCheck(c.wolfSSL_get_error(self.ssl, len));
+        codes.retCheck(c.wolfSSL_get_error(self.ssl, len)) catch |err| {
+            inline for (@typeInfo(ReadError).ErrorSet.?) |e| {
+                const _err = @field(ReadError, e.name);
+                if (err == _err) return _err;
+            }
+            return ReadError.InputOutput;
+        };
     }
     return @intCast(len);
 }
 
-pub fn write(self: *SslConnection, buffer: []const u8) !usize {
+pub fn write(self: *SslConnection, buffer: []const u8) WriteError!usize {
     const len = c.wolfSSL_write(self.ssl, buffer.ptr, @as(c_int, @intCast(buffer.len)));
     if (len < 0) {
-        try codes.retCheck(c.wolfSSL_get_error(self.ssl, len));
+        codes.retCheck(c.wolfSSL_get_error(self.ssl, len)) catch |err| {
+            inline for (@typeInfo(WriteError).ErrorSet.?) |e| {
+                const _err = @field(WriteError, e.name);
+                if (err == _err) return _err;
+            }
+            return WriteError.InputOutput;
+        };
     }
     return @intCast(len);
 }
